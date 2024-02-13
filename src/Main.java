d119d490a31ef317eb0df2b7901b0d5866085910
vlad o <vladoleksiyenko@noreply.github.io>
2024-02-13 09:27:49 -0800

    added ArrayIntList and LinkedIntList classes that implement IntList interface

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+
+        IntList list1 = new ArrayIntList();
+        IntList list2 = new LinkedIntList();
+
+        list1.addBack(42);
+        list1.addBack(82);
+        list1.addBack(97);
+
+        System.out.println(list1);
     }
 }
