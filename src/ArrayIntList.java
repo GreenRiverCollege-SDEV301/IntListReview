e2e0f7ae804c19f4b4f8775463d3c4fd3e2a6c9d
EBoydGRC <EBoydGRC@noreply.github.com>
2024-02-13 10:41:33 -0800

    ArrayIntList implemented and mostly tested.

--- src/ArrayIntList.java
+++ src/ArrayIntList.java
@@ -2,76 +2,151 @@ import java.util.Iterator;
 
 public class ArrayIntList  implements  IntList
 {
+    private int[] buffer;
+    private int size;
+    public ArrayIntList()
+    {
+        buffer = new int[10];
+        size = 0;
+    }
+
     @Override
     public void addFront(int value)
     {
-
+        checkSize();
+        buffer[size] = value;
+        size++;
     }
 
     @Override
     public void addBack(int value)
     {
-
+        checkSize();
+        add(0, value);
     }
 
     @Override
     public void add(int index, int value)
     {
-
+        checkSize();
+        for(int i = size; i > index;  i--)
+        {
+            buffer[i] = buffer[i-1];
+        }
+        buffer[index] = value;
+        size ++;
     }
 
     @Override
     public void removeFront()
     {
-
+        size--;
     }
 
     @Override
     public void removeBack()
     {
-
+        remove(0);
     }
 
     @Override
     public int remove(int index)
     {
-        return 0;
+        checkIndex(index);
+        int returnInt = buffer[index];
+        for(int i = index; i < size;  i++)
+        {
+            buffer[i] = buffer[i + 1];
+        }
+        size --;
+        return returnInt;
     }
 
     @Override
     public int get(int index)
     {
-        return 0;
+        checkIndex(index);
+        return buffer[index];
     }
 
     @Override
     public boolean contains(int value)
     {
-        return false;
+        return indexOf(value) >= 0;
     }
 
     @Override
     public int indexOf(int value)
     {
-        return 0;
+        for (int i = 0; i < size; i++)
+        {
+           if(buffer[i] == value) {
+               return i;
+           }
+        }
+        return -1;
     }
 
     @Override
     public boolean isEmpty()
     {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size()
     {
-        return 0;
+        return size;
     }
 
     @Override
     public void clear()
     {
+        buffer = new int[10];
+        size = 0;
+    }
+    private void checkIndex(int index) throws IndexOutOfBoundsException
+    {
+        if(index >= size || index < 0)
+        {
+            throw new IndexOutOfBoundsException(index + " is outside Array.");
+        }
+    }
 
+    public String toString()
+    {
+        if(size == 0)
+        {
+            return "[]";
+        }
+        else
+        {
+            StringBuilder returnString = new StringBuilder();
+            returnString.append("[").append(buffer[0]);
+            for(int i = 1; i < size; i++)
+            {
+                returnString.append(", ").append(buffer[i]);
+            }
+            returnString.append("]");
+            return returnString.toString();
+        }
+    }
+    private void checkSize()
+    {
+        if(size < buffer.length - 2)
+        {
+            buffer = resize();
+        }
+    }
+
+    private int[] resize() {
+        int[] returnInt = new int[buffer.length * 2];
+
+        for (int i = 0; i < size; i++)
+        {
+            returnInt[i] = buffer[i];
+        }
+        return returnInt;
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,57 @@ public class Main {
     public static void main(String[] args)
     {
         IntList list1 = new ArrayIntList();
-        IntList list2 = new LinkedIntList();
+        //IntList list2 = new LinkedIntList();
 
-        list1.addBack(42);
-        list1.addBack(82);
-        list1.addBack(97);
+        list1.addBack(1);
+        list1.addBack(2);
+        list1.addBack(3);
+        list1.addBack(4);
+        list1.add(2, 5);
+        list1.addFront(9);
+        list1.removeBack();
+        list1.removeFront();
+        list1.clear();
+        list1.addBack(1);
+        list1.addBack(2);
+        list1.addBack(3);
+        list1.addBack(1);
+        list1.addBack(2);
+        list1.addBack(3);
+        list1.addBack(1);
+        list1.addBack(2);
+        list1.addBack(3);
+        list1.addBack(1);
+        list1.addBack(2);
+        list1.addBack(3);
 
         System.out.println(list1);
+
+        System.out.println(list1.contains(2));
+        System.out.println(list1.indexOf(2));
+        System.out.println(list1.contains(4));
+        System.out.println(list1.indexOf(4));
+        System.out.println(list1.size());
+        System.out.println(list1.isEmpty());
+        list1.clear();
+        System.out.println(list1.isEmpty());
+        list1.addBack(5);
+        System.out.println(list1.isEmpty());
+        list1.removeFront();
+        System.out.println(list1.isEmpty());
+
+        list1.addBack(1);
+        list1.addBack(2);
+        list1.addBack(3);
+        list1.addBack(4);
+        list1.add(2, 5);
+        list1.addFront(9);
+        System.out.println(list1);
+        System.out.println(list1.get(2));
+        System.out.println(list1);
+        list1.remove(2);
+        System.out.println(list1);
+
+
     }
 }
