1cd716cd9437847cf2fd3291637fe52e38fdb49b
EBoydGRC <EBoydGRC@noreply.github.com>
2024-02-13 09:30:30 -0800

    First changes to add what implementations we'll be making.

---
+++ src/ArrayIntList.java
@@ -0,0 +1,81 @@
+import java.util.Iterator;
+
+public class ArrayIntList  implements  IntList
+{
+    @Override
+    public void addFront(int value)
+    {
+
+    }
+
+    @Override
+    public void addBack(int value)
+    {
+
+    }
+
+    @Override
+    public void add(int index, int value)
+    {
+
+    }
+
+    @Override
+    public void removeFront()
+    {
+
+    }
+
+    @Override
+    public void removeBack()
+    {
+
+    }
+
+    @Override
+    public int remove(int index)
+    {
+        return 0;
+    }
+
+    @Override
+    public int get(int index)
+    {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(int value)
+    {
+        return false;
+    }
+
+    @Override
+    public int indexOf(int value)
+    {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        return false;
+    }
+
+    @Override
+    public int size()
+    {
+        return 0;
+    }
+
+    @Override
+    public void clear()
+    {
+
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return null;
+    }
+}
