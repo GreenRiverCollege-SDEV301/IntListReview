019e4e8c5aacf62e438954e468535204472f9aaf
cheesy d <cheesyd64@hotmail.com>
2024-03-05 09:52:52 -0800

    Committing main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        IntList list1 = new ArrayIntList();
+        IntList list2 = new LinkedIntList();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        list1.addBack(42);
+        list1.addBack(82);
+        list1.addBack(97);
+        list1.add(1, 500);
+
+        System.out.println("for each loop");
+        for(int value : list1){
+            System.out.println(value);
         }
+
+        System.out.println("iterator");
+        Iterator<Integer> itr = list1.iterator();
+        while(itr.hasNext()){
+            int value = itr.next();
+            System.out.println(value);
+        }
+
+        ///-----------------------------
+        // Linked list test
+        ///----------------------
+        System.out.println("-----------------------");
+
+//        list2.addBack(9);
+//        list2.addBack(7);
+//        list2.addBack(3);
+//        list2.addBack(5);
+
+        list2.addFront(9);
+        list2.addFront(7);
+        list2.addFront(3);
+        list2.addFront(5);
+        System.out.println(list2.toString());
     }
 }
