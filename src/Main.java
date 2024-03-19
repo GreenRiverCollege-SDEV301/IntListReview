3b84c682976732091759ae0004ab50902b0c83d1
Vlad Oleksiyenko <68962947+vladoleksiyenko@users.noreply.github.com>
2024-03-19 15:57:55 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,83 @@ import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
+        IntList list = new ArrayIntList();
         IntList list2 = new LinkedIntList();
+
+        System.out.println("****************************************************");
+        System.out.println("Array List");
+        System.out.println("****************************************************");
+
+        list.addBack(92);
+        list.addBack(24);
+        list.addBack(56);
+        list.addBack(61);
+        list.addBack(756);
+        list.addBack(777);
+        list.addBack(723);
+        list.addBack(947);
+        list.addBack(724);
+        list.addBack(986);
+        list.addBack(1235);
+        list.addBack(3453);
+        list.addBack(8766);
+        list.addBack(3554);
+        list.addBack(1251);
+        System.out.println(list.toString());
+
+        System.out.println("Add front");
+        list.addFront(2);
+        System.out.println("Added 2 to front: --->" + list.toString());
+        System.out.println();
+
+        System.out.println("Add back");
+        list.addBack(2);
+        System.out.println("Added 2 to back: --->" + list.toString());
+        System.out.println();
+
+        System.out.println("Add to index 5");
+        list.add(5, 2);
+        System.out.println("Added 2 to index 5: --->" + list.toString());
+        System.out.println();
+
+        System.out.println("Remove front");
+        list.removeFront();
+        System.out.println("Removed front: --->" + list.toString());
+        System.out.println();
+
+        System.out.println("Remove back");
+        list.removeBack();
+        System.out.println("Removed back: --->" + list.toString());
+        System.out.println();
+
+        System.out.println("Remove index of 4");
+        list.remove(4);
+        System.out.println("Removed back: --->" + list.toString());
+        System.out.println();
+
+        System.out.println("Gets value of index 5");
+        System.out.println("Result: " + list.get(5));
+        System.out.println();
+
+        System.out.println("Contains value 777");
+        System.out.println("Result: " + list.contains(777));
+        System.out.println();
+
+        System.out.println("Index of value 777");
+        System.out.println("Result: " + list.indexOf(777));
+        System.out.println();
+
+        System.out.println("Is empty: " + list.isEmpty());
+        System.out.println("Size: " + list.size());
+        list.clear();
+        System.out.println("Cleared list: -->" + list.toString());
 
         // -------------------------------
         // Linked List Short Test
         // -------------------------------
+        System.out.println("****************************************************");
+        System.out.println("Linked List");
+        System.out.println("****************************************************");
         list2.addBack(92);
         list2.addBack(24);
         list2.addBack(56);
